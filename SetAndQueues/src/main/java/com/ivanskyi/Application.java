03adc9b57b24098e7e3b220327999de26c72bfc3
Oleh <olehfromukraine@gmail.com>
2020-12-25 09:06:36 +0200

    some change

--- SetAndQueues/src/main/java/com/ivanskyi/Application.java
+++ SetAndQueues/src/main/java/com/ivanskyi/Application.java
@@ -8,23 +8,22 @@ public class Application {
         Comparator<Integer> comparator = new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
-                if (o1 % 2 > 0 && o2 % 2 > 0){
+                if (o1 % 2 > 0 && o2 % 2 > 0) {
                     return 1;
-                }
-                else if(o1 % 2 == 0){
-                    if(o2 % 2 == 0 ){
+                } else if(o1 % 2 == 0) {
+                    if(o2 % 2 == 0 ) {
                         return 1;
-                     }
+                        
+                    }
                     return -1;
-                }
-                else{
+                } else {
                     return 1;
                 }
             }
         };
 
         Set<Integer> myTreeSet = new TreeSet<>(comparator);
-        int[] values = {2,1,4,3,5,7,6};
+        int[] values = {1,2,34,4,3,5,7,6,77};
         for(int value : values){
             myTreeSet.add(value);
         }
