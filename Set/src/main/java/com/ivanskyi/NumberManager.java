79dc22f1d1149a2e3c913037a719b3fd400184fe
Oleh <olehfromukraine@gmail.com>
2021-01-28 01:01:25 +0200

    added some change

--- Set/src/main/java/com/ivanskyi/NumberManager.java
+++ Set/src/main/java/com/ivanskyi/NumberManager.java
@@ -1,6 +1,5 @@
 package com.ivanskyi;
 
-
 import java.util.Comparator;
 import java.util.Set;
 import java.util.TreeSet;
@@ -8,19 +7,19 @@ import java.util.TreeSet;
 public class NumberManager {
     static SortByPairsAndGrowth sortByPairsAndGrowth =
             new SortByPairsAndGrowth();
-    static Set<Integer> treeSet = new TreeSet<>(sortByPairsAndGrowth);
+    static Set<Integer> footballPlayer = new TreeSet<>(sortByPairsAndGrowth);
 
     public static void startSort() {
-        treeSet.add(2);
-        treeSet.add(1);
-        treeSet.add(4);
-        treeSet.add(3);
-        treeSet.add(5);
-        treeSet.add(7);
-        treeSet.add(6);
+        footballPlayer.add(2);
+        footballPlayer.add(1);
+        footballPlayer.add(4);
+        footballPlayer.add(3);
+        footballPlayer.add(5);
+        footballPlayer.add(7);
+        footballPlayer.add(6);
 
         System.out.println("Sorted result : ");
-        for(Integer number : treeSet) {
+        for(Integer number : footballPlayer) {
             System.out.print(number + " ");
         }
     }
