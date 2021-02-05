9df5219ff0b79d7a6f3df4832ba364cae4ec4da4
Oleh <olehfromukraine@gmail.com>
2021-02-05 13:13:41 +0200

    Print all bean definition name

--- SimpleSpringBootProject/src/main/java/com/ivanskiy/springboot/Application.java
+++ SimpleSpringBootProject/src/main/java/com/ivanskiy/springboot/Application.java
@@ -2,12 +2,15 @@ package com.ivanskiy.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ApplicationContext context = SpringApplication.run(Application.class, args);
+        for (String beanDefinitionName : context.getBeanDefinitionNames()) {
+            System.out.println(beanDefinitionName);
+        }
     }
-
 }
