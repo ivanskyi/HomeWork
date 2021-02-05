fa5958796fe724da1fe5f002df8eb76d393ff93e
Oleh <olehfromukraine@gmail.com>
2021-02-05 13:56:03 +0200

    add logging.level in application.properties

--- SimpleSpringBootProject/src/main/java/com/ivanskiy/springboot/model/Person.java
+++ SimpleSpringBootProject/src/main/java/com/ivanskiy/springboot/model/Person.java
@@ -1,13 +1,19 @@
 package com.ivanskiy.springboot.model;
 
 import lombok.*;
+import lombok.extern.slf4j.Slf4j;
 
 @Data
 @EqualsAndHashCode
 @ToString(exclude = "{age, address}")
+@Slf4j
 public class Person {
     private String name;
     private String secondName;
     private Long age;
     private String address;
+
+    public void getNameByAddress(String address) {
+        log.info("Person have name {}", name);
+    }
 }
