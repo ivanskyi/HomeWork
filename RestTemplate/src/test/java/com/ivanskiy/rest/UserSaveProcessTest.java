2cc26afedd89d70a6caa38ef6ba413ebb7f3d05e
Oleh <olehfromukraine@gmail.com>
2021-02-27 13:41:43 +0200

    change some code in Test

--- RestTemplate/src/test/java/com/ivanskiy/rest/UserSaveProcessTest.java
+++ RestTemplate/src/test/java/com/ivanskiy/rest/UserSaveProcessTest.java
@@ -9,11 +9,11 @@ import java.net.URL;
 import java.time.LocalDate;
 
 public class UserSaveProcessTest {
-    /*
+
     @Test
     public void getUserInfoByEmail() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        String url = "http://127.0.0.1:8081/find/user/email?email=oleh@site.com";
+        String url = "http://127.0.0.1:8081/user/search?email=alex@site.com";
         User user = objectMapper.readValue(new URL(url), User.class);
         Assert.assertEquals("Alex", user.getName());
         Assert.assertEquals(LocalDate.of(1212, 03, 17), user.getLastLoginDate());
@@ -22,10 +22,9 @@ public class UserSaveProcessTest {
     @Test
     public void checkUserSaveProcess() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        String url = "http://127.0.0.1:8081/new/user?name=Mark&surname=Testman&email=mark@company.com&homework=array\n";
+        String url = " http://127.0.0.1:8081/user/new?name=Oleh&surname=Ivanskiy&" +
+                "lastLoginDate=2021-02-11&email=ol@site.com&homework=generics";
         String status = objectMapper.readValue(new URL(url), String.class);
         Assert.assertEquals("200", status);
     }
-    */
-
 }
